b57a68b11f437380df3b156d605266cd61116096
Uneboss <56261032+Uneboss@users.noreply.github.com>
2020-02-02 01:00:12 +0900

    Create lists.py

---
+++ Python/nomadcoders/Theory/lists.py
@@ -0,0 +1,13 @@
+# Lists_mutable sequence
+days = ["Mon", "Tue", "Wed", "Thur", "Fri"]
+print(type(days))
+print("Mon" in days)
+print(days[3]) #index는 0부터
+print(len(days))
+
+print(days)
+days.append("Sat")
+days.reverse()
+print(days)
+
+# You can add, delete, reverse..etc. anything
