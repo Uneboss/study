7031c73f570f04455194a156487d60409ca77c7b
Uneboss <56261032+Uneboss@users.noreply.github.com>
2020-02-05 00:25:32 +0900

    Create for.py

---
+++ Python/nomadcoders/Theory/for.py
@@ -0,0 +1,15 @@
+# for loop
+# The for loop is used to iterate over the elements of sequence(such as a string, tuple or list) or other iterable object.
+
+# tuple
+days = ("Mon", "Tue", "Wed", "Thur", "Fri")
+
+for x in days:
+  if x is "Wed":
+    break
+  else:
+    print(x)
+
+# string
+for letter in "Uneune":
+  print(letter)
