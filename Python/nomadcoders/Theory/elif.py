c733f21bdc8ca105e3bfeafb54d87f36b9c7d5fc
Uneboss <56261032+Uneboss@users.noreply.github.com>
2020-02-05 00:11:50 +0900

    Create elif.py

---
+++ Python/nomadcoders/Theory/elif.py
@@ -0,0 +1,13 @@
+# if else ( == elif)
+def age_check(age):
+  print(f"you are {age}")
+# use curly braces on variables
+  if age < 18:
+    print("You can't drink")
+  elif age == 18 or age == 19:
+    print("you are new to this!")
+  elif age > 20 and age < 25:
+    print(" you are still kind of young")
+  else:
+    print("enjoy your drink")
+age_check(19)
