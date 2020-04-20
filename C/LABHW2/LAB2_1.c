b604ee11907d281aa4467cc89d6e0314bb7db73f
Uneboss <56261032+Uneboss@users.noreply.github.com>
2020-04-20 23:34:54 +0900

    Create LAB2_1.c

---
+++ C/LABHW2/LAB2_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+    // 단계 1 - 변수 선언 및 값 배정: 키를 나타내는 정수형 변수 height 를 선언하고 값을 배정함.
+    int height = 168;
+    // 단계 2 - 변수를 사용 : 변수 height 를 사용하여 printf 문으로 출력
+    printf("Suehee Pak\t\t%d\n", height);
+    
+    return 0;
+}
